4765724f3cc0d622e90d80f36b5e9f8650784e14
Sidnasciment0 <sid.nas@outlook.com>
2022-02-22 13:33:00 -0300

    Criação dos métodos de adicionar e remover com suas rotas

--- controllers/ContController.js
+++ controllers/ContController.js
@@ -6,8 +6,38 @@ const ContModels = require("../models/ContModels");
 
 class ContController {
 
-
+    static async addCont(req, res){
+        const { value, title, description, opcode } = req.body;
+        let date = req.body.date;
+        date = new Date(date);
+        let filter = new Date(date);
+        const cont = ContModels({date, value, title, description, opcode});
+        await cont.save();
+        res.status(201).json({message: 'Adicionado, deu certo!!'})
     }
+
+    static async removeCont(req, res){ 
+        const {_id} = req.body;
+
+        console.log(_id);
+
+        if(!_id){
+            res.status(401).json({message: `parâmetro-remoção-nulo`});
+            return
+        }
+        const cont = await ContSchema.find({_id:_id});
+        console.log(cont);
+
+        if(cont == null || !cont || cont == ""){
+            res.status(401).json({message: `parâmetro-${_id}-inexistente`});
+            return
+        }
+        
+        await ContSchema.findByIdAndDelete(_id);
+        
+        res.status(202).json({message: `registro- ${_id} - removido`});
+    }
+}
 
 
 module.exports = ContController;
