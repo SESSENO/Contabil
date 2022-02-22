5176d15feec0072d3770c69caedd1c75fe33e424
Sesseno <sfsena@gmail.com>
2022-02-22 15:54:35 -0300

    utlização novos verbos

--- routes/contRouters.js
+++ routes/contRouters.js
@@ -4,13 +4,13 @@ const ReportController = require("../controllers/ReportController");
 const router = Router();
 
 router.post("/add", ContController.addCont);
-router.post("/remove", ContController.removeCont);
+router.delete("/remove", ContController.removeCont);
 
 router.get("/showop/:opcode", ReportController.showOpcode);
 router.get("/reportExp", ReportController.showAvExpenses);
 router.get("/reportRev", ReportController.showAvRevenue);
 router.get("/show", ContController.showCont);
-router.post("/update", ContController.updateCont);
+router.put("/update", ContController.updateCont);
 router.get("/monthReport/:month/:year/:opcode", ReportController.monthReport);
 router.get("/balance/:month/:year", ReportController.balanceReport);
 
