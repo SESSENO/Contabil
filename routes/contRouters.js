ef96514ee1228afaa9718ee8bc58986d87617b3a
silas-sousa <silassousadejesus@gmail.com>
2022-02-22 14:29:38 -0300

    organizando metodos

--- routes/contRouters.js
+++ routes/contRouters.js
@@ -9,6 +9,8 @@ router.post("/remove", ContController.removeCont);
 router.get("/showop/:opcode", ReportController.showOpcode);
 router.get("/reportExp", ReportController.showAvExpenses);
 router.get("/reportRev", ReportController.showAvRevenue);
+router.get("/show", ContController.showCont);
+router.post("/update", ContController.updateCont);
 
 module.exports = router;
 
